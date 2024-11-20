5b1f2187fcc025fe4082c00445e8ef599188cd76
Nachito22 <nachoferre22@gmail.com>
2024-11-19 21:12:50 -0300

    ignore config file

--- utils/ConfigDatabase.py
+++
@@ -1,9 +0,0 @@
-
-def get_config():
-    config = {
-        "user": "root",
-        "password" : "",
-        "host": "localhost",
-        "database": "plataforma_streaming"
-    }
-    return config
