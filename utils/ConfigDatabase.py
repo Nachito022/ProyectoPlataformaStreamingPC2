242babba8daed96e6bb231f53576a8f49139e9b1
Nachito22 <nachoferre22@gmail.com>
2024-11-27 12:18:06 -0300

    comit

---
+++ utils/ConfigDatabase.py
@@ -0,0 +1,10 @@
+#En este archivo se debe configurar los parámetros del respectivo DB
+
+def get_config():
+    config = {
+        "user": "root",
+        "password" : "",
+        "host": "localhost",
+        "database": "plataforma_streaming"
+    }
+    return config
